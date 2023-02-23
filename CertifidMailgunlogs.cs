09d789deb8e312f4e8316c85d4057ec0ae7aaf70
Peter Marsh <42099977+JuanNephrota@users.noreply.github.com>
2023-02-23 16:20:21 -0600

    Add files via upload

---
+++ CertifidMailgunlogs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
+namespace CertifidMailgunlogs
+{
+    public static class CertifidMailgunlogs
+    {
+        [FunctionName("CertifidMailgunlogs")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+    
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogInformation(requestBody);
+
+        }
+    }
+}
+
